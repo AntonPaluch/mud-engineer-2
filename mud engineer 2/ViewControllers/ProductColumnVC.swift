cfce45306a81e5bf71ce6167e2568f28bbff2622
Dmitry Pavlov <perr07@yandex.ru>
2022-09-19 21:32:58 +0300

    lastUpdate

--- mud engineer 2/ViewControllers/ProductColumnVC.swift
+++ mud engineer 2/ViewControllers/ProductColumnVC.swift
@@ -8,9 +8,7 @@
 import UIKit
 
 class ProductColumnVC: UIViewController {
-    
-    private let userDef = UserDefaults.standard
-    
+        
     @IBOutlet weak var longColumn: UITextField!
     @IBOutlet weak var inDiametrColumn: UITextField!
     @IBOutlet weak var wellBottom: UITextField!
@@ -41,14 +39,7 @@ class ProductColumnVC: UIViewController {
         resultButtonOutlet.layer.cornerRadius = 15
         resetButtonOutlet.layer.cornerRadius = resetButtonOutlet.frame.width / 2
         resetButtonOutlet.layer.masksToBounds = true
-        longColumn.text = userDef.string(forKey: "c1")
-        inDiametrColumn.text = userDef.string(forKey: "c2")
-        wellBottom.text = userDef.string(forKey: "c3")
-        diametrDrilling.text = userDef.string(forKey: "c4")
-        kCavernosity.text = userDef.string(forKey: "c5")
-        diametrDrillingPipes.text = userDef.string(forKey: "c6")
-        wallThickness.text = userDef.string(forKey: "c7")
-        pumpLiters.text = userDef.string(forKey: "c8")
+        presentValue()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -57,112 +48,16 @@ class ProductColumnVC: UIViewController {
     }
     
     @IBAction func resultButton(_ sender: Any) {
-        
-        guard wellBottom.text != "" else {
-            showAlert(
-            title: NSLocalizedString("bottomWell", comment: "")
-        )
-        return
-        }
-            
-        guard diametrDrilling.text != "" else {
-                showAlert(
-                title: NSLocalizedString("bit diameter", comment: "")
-        )
-        return
-        }
-            
-        guard kCavernosity.text != "" else {
-                showAlert(
-                title: NSLocalizedString("kCavernosity", comment: "")
-            )
-        return
-        }
-            
-        guard diametrDrillingPipes.text != "" else {
-                showAlert(
-                title: NSLocalizedString("dSDP", comment: "")
-        )
-        return
-        }
-        
-        guard wallThickness.text != "" else {
-                showAlert(
-                    title: NSLocalizedString("pipeWall", comment: "")
-        )
-        return
-        }
-        
-        guard pumpLiters.text != "" else {
-                showAlert(
-                title: NSLocalizedString("liter", comment: "")
-        )
-        return
-        }
-            
-        guard let pumpLitersString = pumpLiters.text,
-        let pumpLitersDouble = Double(pumpLitersString) else { return }
-    
-        volumeColumn = CalculationManager().vKolonny(
-        vnDiametrKolonni: inDiametrColumn.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
-        dlinaKolonni: longColumn.text?.replacingOccurrences(of: ",", with: ".") ?? "1"
-    )
-        volumeOpenBorehole = CalculationManager().vOtkritiStvol(
-        dDolota: diametrDrilling.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
-        zaboy: wellBottom.text?.replacingOccurrences(of: ",", with: ".") ?? "1" ,
-        dlinaKolonni: longColumn.text ?? "1",
-        kKavernoznosty: kCavernosity.text ?? "1"
-    )
-        volumePipe = CalculationManager().metalSbt(
-        dInstrumenta: diametrDrillingPipes.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
-        stenkaSbt: wallThickness.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
-        zaboy: wellBottom.text?.replacingOccurrences(of: ",", with: ".") ?? "1"
-    )
-        volumeTotal = (Double(volumeColumn) ?? 1.0) + (Double(volumeOpenBorehole) ?? 1.0)
-        volumeincludingPipes = volumeTotal - (Double(volumePipe) ?? 1.0)
-        volumeInPipes = CalculationManager().vRastvoraVtrubax(
-        dInstrumenta: diametrDrillingPipes.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
-        stenkaSbt: wallThickness.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
-        zaboy: wellBottom.text?.replacingOccurrences(of: ",", with: ".") ?? "1"
-    )
-        volumeBehindPipes = volumeincludingPipes - (Double(volumeInPipes) ?? 1.0)
-        outputDownholePack = Double(volumeincludingPipes) / ((pumpLitersDouble * 60) / 1000)
-        pumpingToBottomWell = (Double(volumeInPipes) ?? 1.0) / ((pumpLitersDouble * 60) / 1000)
-        wellFlushingCycle = outputDownholePack + pumpingToBottomWell
-        wellFlushingCycleOneHalf = wellFlushingCycle * 1.5
-        wellFlushingCycleTwo = wellFlushingCycle * 2
-        
-        userDef.setValue(longColumn.text, forKey: "c1")
-        userDef.setValue(inDiametrColumn.text, forKey: "c2")
-        userDef.setValue(wellBottom.text, forKey: "c3")
-        userDef.setValue(diametrDrilling.text, forKey: "c4")
-        userDef.setValue(kCavernosity.text, forKey: "c5")
-        userDef.setValue(diametrDrillingPipes.text, forKey: "c6")
-        userDef.setValue(wallThickness.text, forKey: "c7")
-        userDef.setValue(pumpLiters.text, forKey: "c8")
+        presentAlert()
+        allResult()
+        saveValue()
         
         performSegue(withIdentifier: "column", sender: nil)
     }
     
     
     @IBAction func resetButton(_ sender: UIButton) {
-        longColumn.text = ""
-        inDiametrColumn.text = ""
-        wellBottom.text = ""
-        diametrDrilling.text = ""
-        kCavernosity.text = ""
-        diametrDrillingPipes.text = ""
-        wallThickness.text = ""
-        pumpLiters.text = ""
-        
-        userDef.removeObject(forKey: "c1")
-        userDef.removeObject(forKey: "c2")
-        userDef.removeObject(forKey: "c3")
-        userDef.removeObject(forKey: "c4")
-        userDef.removeObject(forKey: "c5")
-        userDef.removeObject(forKey: "c6")
-        userDef.removeObject(forKey: "c7")
-        userDef.removeObject(forKey: "c8")
+        resetValue()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -194,6 +89,59 @@ extension ProductColumnVC {
         }
         alert.addAction(okAction)
         present(alert, animated: true)
+    }
+    
+    func presentAlert() {
+        guard wellBottom.text != "" else {showAlert(title: NSLocalizedString("bottomWell", comment: ""))
+           return }
+        guard diametrDrilling.text != "" else { showAlert(title: NSLocalizedString("bit diameter", comment: ""))
+           return }
+        guard kCavernosity.text != "" else {showAlert(title: NSLocalizedString("kCavernosity", comment: ""))
+           return }
+        guard diametrDrillingPipes.text != "" else {showAlert(title: NSLocalizedString("dSDP", comment: ""))
+           return }
+        guard wallThickness.text != "" else {showAlert(title: NSLocalizedString("pipeWall", comment: ""))
+           return }
+        guard pumpLiters.text != "" else {showAlert(title: NSLocalizedString("liter", comment: ""))
+           return}
+    }
+}
+
+// MARK: - allResult
+
+extension ProductColumnVC {
+    func allResult() {
+    guard let pumpLitersString = pumpLiters.text,
+    let pumpLitersDouble = Double(pumpLitersString) else { return }
+
+    volumeColumn = CalculationManager().vKolonny(
+    vnDiametrKolonni: inDiametrColumn.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
+    dlinaKolonni: longColumn.text?.replacingOccurrences(of: ",", with: ".") ?? "1"
+)
+    volumeOpenBorehole = CalculationManager().vOtkritiStvol(
+    dDolota: diametrDrilling.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
+    zaboy: wellBottom.text?.replacingOccurrences(of: ",", with: ".") ?? "1" ,
+    dlinaKolonni: longColumn.text ?? "1",
+    kKavernoznosty: kCavernosity.text ?? "1"
+)
+    volumePipe = CalculationManager().metalSbt(
+    dInstrumenta: diametrDrillingPipes.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
+    stenkaSbt: wallThickness.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
+    zaboy: wellBottom.text?.replacingOccurrences(of: ",", with: ".") ?? "1"
+)
+    volumeTotal = (Double(volumeColumn) ?? 1.0) + (Double(volumeOpenBorehole) ?? 1.0)
+    volumeincludingPipes = volumeTotal - (Double(volumePipe) ?? 1.0)
+    volumeInPipes = CalculationManager().vRastvoraVtrubax(
+    dInstrumenta: diametrDrillingPipes.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
+    stenkaSbt: wallThickness.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
+    zaboy: wellBottom.text?.replacingOccurrences(of: ",", with: ".") ?? "1"
+)
+    volumeBehindPipes = volumeincludingPipes - (Double(volumeInPipes) ?? 1.0)
+    outputDownholePack = Double(volumeincludingPipes) / ((pumpLitersDouble * 60) / 1000)
+    pumpingToBottomWell = (Double(volumeInPipes) ?? 1.0) / ((pumpLitersDouble * 60) / 1000)
+    wellFlushingCycle = outputDownholePack + pumpingToBottomWell
+    wellFlushingCycleOneHalf = wellFlushingCycle * 1.5
+    wellFlushingCycleTwo = wellFlushingCycle * 2
     }
 }
 
--- mud engineer 2/ViewControllers/ShankVC.swift
+++ mud engineer 2/ViewControllers/ShankVC.swift
@@ -8,9 +8,7 @@
 import UIKit
 
 class ShankVC: UIViewController {
-    
-    private let userDef = UserDefaults.standard
-    
+        
     @IBOutlet weak var longColumn: UITextField!
     @IBOutlet weak var inDiametrColumn: UITextField!
     @IBOutlet weak var wellBottom: UITextField!
@@ -41,14 +39,7 @@ class ShankVC: UIViewController {
         resultButtonOutlet.layer.cornerRadius = 15
         resetOutlet.layer.cornerRadius = resetOutlet.frame.width / 2
         resetOutlet.layer.masksToBounds = true
-        longColumn.text = userDef.string(forKey: "s1")
-        inDiametrColumn.text = userDef.string(forKey: "s2")
-        wellBottom.text = userDef.string(forKey: "s3")
-        diametrDrilling.text = userDef.string(forKey: "s4")
-        kCavernosity.text = userDef.string(forKey: "s5")
-        diametrDrillingPipes.text = userDef.string(forKey: "s6")
-        wallThickness.text = userDef.string(forKey: "s7")
-        pumpLiters.text = userDef.string(forKey: "s8")
+        presentValue()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -57,112 +48,15 @@ class ShankVC: UIViewController {
     }
    
     @IBAction func resultButton(_ sender: UIButton) {
-        
-        guard wellBottom.text != "" else {
-            showAlert(
-            title: NSLocalizedString("bottomWell", comment: "")
-        )
-        return
-        }
-            
-        guard diametrDrilling.text != "" else {
-                showAlert(
-                title: NSLocalizedString("bit diameter", comment: "")
-        )
-        return
-        }
-            
-        guard kCavernosity.text != "" else {
-                showAlert(
-                title: NSLocalizedString("kCavernosity", comment: "")
-            )
-        return
-        }
-            
-        guard diametrDrillingPipes.text != "" else {
-                showAlert(
-                title: NSLocalizedString("dSDP", comment: "")
-        )
-        return
-        }
-        
-        guard wallThickness.text != "" else {
-                showAlert(
-                title: NSLocalizedString("pipeWall", comment: "")
-        )
-        return
-        }
-        
-        guard pumpLiters.text != "" else {
-                showAlert(
-                title: NSLocalizedString("liter", comment: "")
-        )
-        return
-        }
-        
-        guard let pumpLitersString = pumpLiters.text,
-        let pumpLitersDouble = Double(pumpLitersString) else { return }
-    
-        volumeColumn = CalculationManager().vKolonny(
-        vnDiametrKolonni: inDiametrColumn.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
-        dlinaKolonni: longColumn.text?.replacingOccurrences(of: ",", with: ".") ?? "1"
-    )
-        volumeOpenBorehole = CalculationManager().vOtkritiStvol(
-        dDolota: diametrDrilling.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
-        zaboy: wellBottom.text?.replacingOccurrences(of: ",", with: ".") ?? "1" ,
-        dlinaKolonni: longColumn.text ?? "1",
-        kKavernoznosty: kCavernosity.text ?? "1"
-    )
-        volumePipe = CalculationManager().metalSbt(
-        dInstrumenta: diametrDrillingPipes.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
-        stenkaSbt: wallThickness.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
-        zaboy: wellBottom.text?.replacingOccurrences(of: ",", with: ".") ?? "1"
-    )
-        volumeTotal = (Double(volumeColumn) ?? 1.0) + (Double(volumeOpenBorehole) ?? 1.0)
-        volumeincludingPipes = volumeTotal - (Double(volumePipe) ?? 1.0)
-        volumeInPipes = CalculationManager().vRastvoraVtrubax(
-        dInstrumenta: diametrDrillingPipes.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
-        stenkaSbt: wallThickness.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
-        zaboy: wellBottom.text?.replacingOccurrences(of: ",", with: ".") ?? "1"
-    )
-        volumeBehindPipes = volumeincludingPipes - (Double(volumeInPipes) ?? 1.0)
-        outputDownholePack = Double(volumeincludingPipes) / ((pumpLitersDouble * 60) / 1000)
-        pumpingToBottomWell = (Double(volumeInPipes) ?? 1.0) / ((pumpLitersDouble * 60) / 1000)
-        wellFlushingCycle = outputDownholePack + pumpingToBottomWell
-        wellFlushingCycleOneHalf = wellFlushingCycle * 1.5
-        wellFlushingCycleTwo = wellFlushingCycle * 2
-        
-        userDef.setValue(longColumn.text, forKey: "s1")
-        userDef.setValue(inDiametrColumn.text, forKey: "s2")
-        userDef.setValue(wellBottom.text, forKey: "s3")
-        userDef.setValue(diametrDrilling.text, forKey: "s4")
-        userDef.setValue(kCavernosity.text, forKey: "s5")
-        userDef.setValue(diametrDrillingPipes.text, forKey: "s6")
-        userDef.setValue(wallThickness.text, forKey: "s7")
-        userDef.setValue(pumpLiters.text, forKey: "s8")
+        presentAlert()
+        allResult()
+        saveValue()
         
         performSegue(withIdentifier: "shank", sender: nil)
-        
     }
     
     @IBAction func resetButton(_ sender: UIButton) {
-        longColumn.text = ""
-        inDiametrColumn.text = ""
-        wellBottom.text = ""
-        diametrDrilling.text = ""
-        kCavernosity.text = ""
-        diametrDrillingPipes.text = ""
-        wallThickness.text = ""
-        pumpLiters.text = ""
-        
-        userDef.removeObject(forKey: "s1")
-        userDef.removeObject(forKey: "s2")
-        userDef.removeObject(forKey: "s3")
-        userDef.removeObject(forKey: "s4")
-        userDef.removeObject(forKey: "s5")
-        userDef.removeObject(forKey: "s6")
-        userDef.removeObject(forKey: "s7")
-        userDef.removeObject(forKey: "s8")
+        resetValue()
     }
     
         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -193,4 +87,56 @@ extension ShankVC {
         alert.addAction(okAction)
         present(alert, animated: true)
     }
+    
+    func presentAlert() {
+        guard wellBottom.text != "" else {showAlert(title: NSLocalizedString("bottomWell", comment: ""))
+           return }
+        guard diametrDrilling.text != "" else {showAlert(title: NSLocalizedString("bit diameter", comment: ""))
+           return }
+        guard kCavernosity.text != "" else {showAlert(title: NSLocalizedString("kCavernosity", comment: ""))
+           return }
+        guard diametrDrillingPipes.text != "" else {showAlert(title: NSLocalizedString("dSDP", comment: ""))
+           return }
+        guard wallThickness.text != "" else {showAlert(title: NSLocalizedString("pipeWall", comment: ""))
+           return }
+        guard pumpLiters.text != "" else {showAlert(title: NSLocalizedString("liter", comment: ""))
+           return}
+    }
+}
+
+// MARK: - allResult
+
+extension ShankVC {
+    func allResult() {
+        guard let pumpLitersString = pumpLiters.text,
+        let pumpLitersDouble = Double(pumpLitersString) else { return }
+        volumeColumn = CalculationManager().vKolonny(
+        vnDiametrKolonni: inDiametrColumn.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
+        dlinaKolonni: longColumn.text?.replacingOccurrences(of: ",", with: ".") ?? "1"
+    )
+        volumeOpenBorehole = CalculationManager().vOtkritiStvol(
+        dDolota: diametrDrilling.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
+        zaboy: wellBottom.text?.replacingOccurrences(of: ",", with: ".") ?? "1" ,
+        dlinaKolonni: longColumn.text ?? "1",
+        kKavernoznosty: kCavernosity.text ?? "1"
+    )
+        volumePipe = CalculationManager().metalSbt(
+        dInstrumenta: diametrDrillingPipes.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
+        stenkaSbt: wallThickness.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
+        zaboy: wellBottom.text?.replacingOccurrences(of: ",", with: ".") ?? "1"
+    )
+        volumeTotal = (Double(volumeColumn) ?? 1.0) + (Double(volumeOpenBorehole) ?? 1.0)
+        volumeincludingPipes = volumeTotal - (Double(volumePipe) ?? 1.0)
+        volumeInPipes = CalculationManager().vRastvoraVtrubax(
+        dInstrumenta: diametrDrillingPipes.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
+        stenkaSbt: wallThickness.text?.replacingOccurrences(of: ",", with: ".") ?? "1",
+        zaboy: wellBottom.text?.replacingOccurrences(of: ",", with: ".") ?? "1"
+    )
+        volumeBehindPipes = volumeincludingPipes - (Double(volumeInPipes) ?? 1.0)
+        outputDownholePack = Double(volumeincludingPipes) / ((pumpLitersDouble * 60) / 1000)
+        pumpingToBottomWell = (Double(volumeInPipes) ?? 1.0) / ((pumpLitersDouble * 60) / 1000)
+        wellFlushingCycle = outputDownholePack + pumpingToBottomWell
+        wellFlushingCycleOneHalf = wellFlushingCycle * 1.5
+        wellFlushingCycleTwo = wellFlushingCycle * 2
+  }
 }
